7d008594e6f9d6541bf8d1f43691110709e8c2f4
xyvie lyons <xyvielyons@gmail.com>
2024-06-25 03:08:42 +0300

    products CRUD

--- Controllers/Inventory/products.controller.js
+++ Controllers/Inventory/products.controller.js
@@ -1,3 +1,96 @@
-export const InventoryProductsResolvers = {
+import ProductsModel from "../../models/InventoryModels/Products.model.js";
 
+export const InventoryProductsResolvers = {
+    Query:{
+        products:async(obj,args,context)=>{
+           
+                const getallproducts = await ProductsModel.find({userId:args.userId})
+                if(!getallproducts){
+                  return new Error("error fetching products")
+                }
+                return getallproducts;
+
+            
+            
+        },
+
+        product:async(obj,args,context)=>{
+      
+                const getsingleproduct = await ProductsModel.findById(args.id)
+                if(!getsingleproduct){
+                    return new Error('error fetching product')
+                }
+
+
+                return getsingleproduct;
+                
+         
+        }
+
+    },
+    Mutation:{
+        updateProduct:async(obj,args,context)=>{
+                const getProduct= await ProductsModel.find({id:args.id,userId:args.userId})
+
+                if(!getProduct){
+                    return new Error("the product was not found")
+                }
+            
+                const updateproduct = await ProductsModel.findByIdAndUpdate(args.id,{
+                    product:args.product,
+                    image:args.image,
+                    type:args.type,
+                    category:args.category,
+                    brand:args.brand,
+                    salePrice:args.salePrice,
+                    purchasePrice:args.purchasePrice,
+                    stockUnit:args.stockUnit,
+                    warehouse:args.warehouse
+
+                })
+                if(!updateproduct){
+                  return new Error('error updating products')
+                }
+
+                const getUpdatedProduct = await ProductsModel.findById(args.id)
+                
+                return getUpdatedProduct
+
+           
+          
+
+
+        },
+        createProduct:async(obj,args,context)=>{
+           
+
+                const createProduct = new ProductsModel({
+                    userId:args.userId,
+                    product:args.product,
+                    image:args.image,
+                    type:args.type,
+                    category:args.category,
+                    brand:args.brand,
+                    salePrice:args.salePrice,
+                    purchasePrice:args.purchasePrice,
+                    stockUnit:args.stockUnit,
+                    warehouse:args.warehouse
+    
+                })
+               
+                await createProduct.save()
+                
+                if(!createProduct){
+                    return new Error('error creating product')
+                }
+    
+                return createProduct
+
+
+           
+            
+
+        }
+
+    }
 }
--- Graphql/InventorySchemas/ProductsSchema.js
+++ Graphql/InventorySchemas/ProductsSchema.js
@@ -1,16 +1,28 @@
 export const InventoryProductsTypeDef = /* GraphQL */ `
     type Products {
         id:ID!
-        product:String
-        image:String
-        type:String
-        category:String
-        brand:String
-        salePrice:Float
-        purchasePrice:Float
-        stockUnit:Float
-        warehouse:String
+        userId:String!
+        product:String!
+        image:String!
+        type:String!
+        category:String!
+        brand:String!
+        salePrice:Float!
+        purchasePrice:Float!
+        stockUnit:Float!
+        warehouse:String!
     }
+
+    type Query {
+        product(id:ID!):Products
+        products(userId:String!):[Products]
+    }
+    type Mutation{
+        updateProduct(id:ID!,userId:String!,product:String,image:String,type:String,category:String,brand:String,salePrice:Float,purchasePrice:Float,stockUnit:Float,warehouse:String):Products
+        createProduct(userId:String!,product:String,image:String,type:String,category:String,brand:String,salePrice:Float,purchasePrice:Float,stockUnit:Float,warehouse:String):Products
+
+    }
+
 
 
 `
